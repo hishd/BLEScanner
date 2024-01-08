9a891059e6f2666865dc08eff5ed24c4d159c984
HishD <hisharadilshan3@gmail.com>
2024-01-08 18:21:48 +0000

    Initial Commit

---
+++ BLEScanner/BLEScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEScannerApp.swift
+//  BLEScanner
+//
+//  Created by Hishara Dilshan on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct BLEScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
